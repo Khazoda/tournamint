f252e849dd31f54e2869916edb1cc3f3657a3e19
June <khazoda@gmail.com>
2022-03-29 02:57:01 +0100

    navbar

---
+++ components/common/Template.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Template({}: Props) {
+  return <div>Template</div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/common/Navbar'
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center"></main>
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <Navbar></Navbar>
+      </main>
     </div>
   )
 }
