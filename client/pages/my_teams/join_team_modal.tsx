248b379a077a1bfa4aef4f1a8a6fc230642a8711
June <khazoda@gmail.com>
2022-07-02 18:48:56 +0100

    basic team join implemented

--- client/pages/my_teams/join_team_modal.tsx
+++ client/pages/my_teams/join_team_modal.tsx
@@ -25,6 +25,7 @@ const JoinTeamModal = (props: Props) => {
   } = useUser()
 
   const TEST = async (tag: string) => {
+    tag = tag.toUpperCase()
     const url = '/api/teamData?' + new URLSearchParams({ team_tag: tag })
     const result = await fetch(url)
       .then((res) => res.json())
@@ -32,6 +33,7 @@ const JoinTeamModal = (props: Props) => {
 
     console.log('getUserTeam(): ', result)
   }
+
   const joinTeam = async (tag: string) => {
     tag = tag.toUpperCase()
     console.log('TAG:', tag)
@@ -41,13 +43,50 @@ const JoinTeamModal = (props: Props) => {
       .then((res) => res.json())
       .catch((res) => console.log(res.error))
 
+    let team_temp = result.response
     if (team != null) {
       alert('You are already in a team.')
     } else {
       if (result.response == null) {
         alert('Invalid Team Tag.')
       } else {
-        alert(result.response.team_tag + ' joined.')
+        if (team_temp.team_owner == ign) {
+          alert('you are the team owner, how did you even access this page? 🤔')
+        } else {
+          if (!team_temp.team_members.includes(ign)) {
+            team_temp.team_members.push(ign)
+            const response = await fetch('/api/teamData', {
+              body: JSON.stringify({ data: team_temp }),
+              headers: { 'Content-Type': 'application/json' },
+              method: 'PATCH',
+            })
+            localStorage.setItem(
+              'userDetails',
+              JSON.stringify({
+                displayName: displayName,
+                biography: biography,
+                ign: ign,
+                favouriteChampion: favouriteChampion,
+                rankInfo: {
+                  tier: rankInfo.tier,
+                  rank: rankInfo.rank,
+                  wins: rankInfo.wins,
+                  losses: rankInfo.losses,
+                },
+                statistics: {
+                  tournaments_played: statistics.tournaments_played,
+                  tournaments_won: statistics.tournaments_won,
+                  matches_won: statistics.matches_won,
+                  people_met: statistics.people_met,
+                },
+                tournamentsMade: tournamentsMade,
+                tournaments: tournaments,
+                team: team_temp,
+              })
+            )
+            console.log('Team Join Status:', response.status)
+          }
+        }
       }
     }
   }
