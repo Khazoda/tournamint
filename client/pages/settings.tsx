4b10a650fb09b4503094e3ed113405b217daeb2b
June <khazoda@gmail.com>
2022-07-18 22:30:09 +0100

    settings delete account footer

--- client/pages/settings.tsx
+++ client/pages/settings.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../components/common/Button'
 
 type Props = {}
 
@@ -8,15 +9,34 @@ function Settings({}: Props) {
       id="wrapper"
       className="container mx-auto h-full min-h-screen overflow-y-auto px-4 pt-24 pb-4"
     >
-      <article className="prose mx-auto">
-        <h1>Settings</h1>
-        <div className="align-center flex flex-row items-center justify-between">
-          <label>Setting 1</label>
-          <input type="checkbox" className="toggle" />
+      <article className="prose prose-slate mx-auto flex h-full flex-col justify-between text-black-700 dark:text-white-200">
+        {/* Settings */}
+        <div>
+          <h1 className="text-black-700 dark:text-white-200">Settings</h1>
+          <div className="align-center flex flex-row items-center justify-between">
+            <label>Setting 1</label>
+            <input type="checkbox" className="toggle" />
+          </div>
         </div>
-        <div className="align-center flex flex-row items-center justify-between">
-          <label>Setting 2</label>
-          <input type="checkbox" className="toggle" />
+
+        {/* Footer */}
+        <div className="">
+          <div className="divider dark:before:bg-white-800 dark:after:bg-white-800"></div>
+          <div className="align-center flex flex-row items-center justify-between ">
+            <label>Delete Account</label>
+            <Button
+              text="DELETE"
+              type="negative"
+              fixedWidth
+              onClick={() => alert('TODO: Implement account deletion')}
+            ></Button>
+          </div>
+          <ul>
+            <li className="text-gray-500 dark:text-gray-400">
+              Deleting your account removes all identifiable user data
+              associated with you from this Tournamint distribution.
+            </li>
+          </ul>
         </div>
       </article>
     </div>
