fb8a0737811c4ccde8a1f04b93b70705bed18cda
June <khazoda@gmail.com>
2022-08-09 16:16:00 +0100

    filling up team style additions

--- client/components/common/TournamentDisplay/TournamentFillingUp.tsx
+++ client/components/common/TournamentDisplay/TournamentFillingUp.tsx
@@ -22,6 +22,11 @@ const TournamentFillingUp = (props: {
         tournamentsMade,
     } = useUser()
 
+    let userTeam: ITeam
+    if (team != null) {
+        userTeam = team
+    }
+
     const [fresh_tournament_data, setFresh_tournament_data] = useState<ITournament>()
     const [riot_data, setRiot_data] = useState<any>({})
     let empty_spots_counter: number[] = [1, 1, 1, 1, 1]
@@ -81,67 +86,66 @@ const TournamentFillingUp = (props: {
     return (
         <>
             {/* <div>{fresh_tournament_data != undefined && fresh_tournament_data.teams.length} / {props.tournament.type} teams have joined</div> */}
-            <div className="mt-4 text-lg">The tournament is not full yet. Please wait for more teams to join.</div>
-            <div className="w-full h-full flex flex-row flex-wrap gap-4 mt-4">
+            <div className="mt-4 text-lg text-yellow-600 dark:text-yellow-400">The tournament is not full yet. Please wait for more teams to join.</div>
+            <div className="w-full h-full flex flex-row flex-wrap gap-4 mt-8">
                 {fresh_tournament_data != undefined && fresh_tournament_data.teams.map((team: ITeam) => {
 
                     return (
-                        <div key={team.team_tag} style={{ borderColor: team.team_colour_hex }} className={" border-2 card w-48 h-64 bg-base-100 dark:bg-gray-700 shadow-xl"}>
-                            <div className=" w-full h-24 flex flex-row items-center justify-center bg-black-400 drop-shadow-lg">
-                                <img src={logos[team.team_icon_path].src} alt="team_icon" className="rounded-xl w-20 h-20" />
-                                <h1 style={{ color: team.team_colour_hex }} className="card-title text-3xl tracking-widest drop-shadow-lg">{team.team_tag}</h1>
-                            </div>
-                            <div className="card-body items-center text-center p-2">
-                                <h1 className="card-title text-xl max-w-[10rem] text-ellipsis overflow-hidden inline" title={team.team_name}>{team.team_name}</h1>
+                        <div className="relative">
+                            <img src={logos[team.team_icon_path].src} alt="team_icon" className="rounded-xl w-16 h-16 absolute -top-10 left-1/2 -translate-x-1/2 z-50" />
+                            <div key={team.team_tag} style={team.team_tag == userTeam.team_tag ? { borderColor: 'lime', borderWidth: '4px' } : { borderColor: team.team_colour_hex }} className={" border-2 card w-24 h-36 bg-base-100 dark:bg-gray-700 shadow-xl"}>
+                                <h1 title={team.team_name} style={{ color: team.team_colour_hex }} className="card-title text-3xl tracking-widest drop-shadow-lg mx-auto pt-2 cursor-default hover:scale-105">{team.team_tag}</h1>
+                                <div className="card-body items-center text-center p-2">
+                                    {/* <h1 className="card-title text-xl max-w-[10rem] text-ellipsis overflow-hidden inline" title={team.team_name}>{team.team_name}</h1> */}
 
-                                {riot_data[team.team_tag] != undefined ?
-                                    <div
-                                        title="View Profile"
-                                        className="group relative inline h-[68px] w-[68px] border-2 border-green-500 transition-[border] hover:cursor-pointer hover:border-green-800 md:h-[60px] md:w-[60px] "
-                                    >
-                                        <Image
-                                            src={
-                                                riot_data[team.team_tag].profileIconId === undefined
-                                                    ? '/images/spinner.svg'
-                                                    : DD_PREFIX +
-                                                    'img/profileicon/' +
-                                                    riot_data[team.team_tag].profileIconId +
-                                                    '.png'
-                                            }
-                                            alt="Profile picture"
-                                            layout="fill"
-                                            objectFit="cover"
-                                            className=""
-                                        ></Image>
-                                        <span className="absolute -bottom-3 left-1/2 w-3/4 -translate-x-1/2 rounded-md border-2 border-green-500 bg-gray-800 px-2 text-center text-sm text-white-200 transition-[border] group-hover:border-green-800">
-                                            {riot_data[team.team_tag].summonerLevel === undefined
-                                                ? '666'
-                                                : riot_data[team.team_tag].summonerLevel}
-                                        </span>
-                                    </div>
-                                    :
-                                    <div
-                                        title="View Profile"
-                                        className="group relative inline h-[68px] w-[68px] border-2 border-green-500 transition-[border] hover:cursor-pointer hover:border-green-800 md:h-[60px] md:w-[60px] "
-                                    >
-                                        <Image
-                                            src={
-                                                '/images/spinner.svg'
+                                    {riot_data[team.team_tag] != undefined ?
+                                        <div
+                                            title={team.team_owner}
+                                            className="group hover:scale-105 relative inline h-[68px] w-[68px] border-2 border-green-500 transition-[border]  md:h-[60px] md:w-[60px] "
+                                        >
+                                            <Image
+                                                src={
+                                                    riot_data[team.team_tag].profileIconId === undefined
+                                                        ? '/images/spinner.svg'
+                                                        : DD_PREFIX +
+                                                        'img/profileicon/' +
+                                                        riot_data[team.team_tag].profileIconId +
+                                                        '.png'
+                                                }
+                                                alt="Profile picture"
+                                                layout="fill"
+                                                objectFit="cover"
+                                                className=""
+                                            ></Image>
+                                            <span className="absolute -bottom-3 left-1/2 w-3/4 -translate-x-1/2 rounded-md border-2 border-green-500 bg-gray-800 px-2 text-center text-sm text-white-200 transition-[border] ">
+                                                {riot_data[team.team_tag].summonerLevel === undefined
+                                                    ? '666'
+                                                    : riot_data[team.team_tag].summonerLevel}
+                                            </span>
+                                        </div>
+                                        :
+                                        <div
+                                            title="View Profile"
+                                            className="group relative inline h-[68px] w-[68px] border-2 border-green-500 transition-[border] md:h-[60px] md:w-[60px] "
+                                        >
+                                            <Image
+                                                src={
+                                                    '/images/spinner.svg'
 
-                                            }
-                                            alt="Profile picture"
-                                            layout="fill"
-                                            objectFit="cover"
-                                            className=""
-                                        ></Image>
-                                        <span className="absolute -bottom-3 left-1/2 w-3/4 -translate-x-1/2 rounded-md border-2 border-green-500 bg-gray-800 px-2 text-center text-sm text-white-200 transition-[border] group-hover:border-green-800">
-                                            666
-                                        </span>
-                                    </div>
+                                                }
+                                                alt="Profile picture"
+                                                layout="fill"
+                                                objectFit="cover"
+                                                className=""
+                                            ></Image>
+                                            <span className="absolute -bottom-3 left-1/2 w-3/4 -translate-x-1/2 rounded-md border-2 border-green-500 bg-gray-800 px-2 text-center text-sm text-white-200 transition-[border] ">
+                                                666
+                                            </span>
+                                        </div>
 
-                                }
-                                <div className="block pt-2">{team.team_owner}</div>
+                                    }
 
+                                </div>
                             </div>
                         </div>
                     )
@@ -149,7 +153,7 @@ const TournamentFillingUp = (props: {
                 {
                     fresh_tournament_data != undefined &&
                     [...Array(props.tournament.type - fresh_tournament_data.teams.length)].map((e, i) =>
-                        <div key={i} style={{ borderColor: 'gray' }} className={" border-2 card w-48 h-64 bg-base-100 dark:bg-gray-900 shadow-xl"}>
+                        <div key={i} style={{ borderColor: 'gray' }} className={" border-2 card w-24 h-36 bg-base-100 dark:bg-gray-900 shadow-xl"}>
                         </div>
                     )
                 }
