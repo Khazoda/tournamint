8e58aba014d268f4a932120ad283ea3adf2772d0
June <khazoda@gmail.com>
2022-09-07 18:19:55 +0100

    datadragon fix

--- client/globals/riot_consts.tsx
+++ client/globals/riot_consts.tsx
@@ -1,5 +1,5 @@
 // Data Dragon Prefix - update every patch
 const VERSION_JSON = 'https://ddragon.leagueoflegends.com/api/versions.json'
-export const DD_PREFIX = 'http://ddragon.leagueoflegends.com/cdn/12.16.1'
+export const DD_PREFIX = 'http://ddragon.leagueoflegends.com/cdn/12.16.1/'
 // Tournamint API key
 export const API_KEY = 'RGAPI-a65ff374-7f9b-4aed-9b7a-fadf87277db8'
